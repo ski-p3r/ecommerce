ef9db9b1981f104db3e9bbf415e68415f43690c6
Mubarek Shikur <manmuba90@gmail.com>
2023-09-30 11:00:23 +0300

    adding template for email verification

--- api/settings.py
+++ api/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
